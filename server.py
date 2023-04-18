fead13d0baabb51681a4a6d16048afc09943d5a7
Chengzhong Luo <luoc@usc.edu>
2023-04-17 18:30:08 -0700

    changes

--- server.py
+++ server.py
@@ -12,13 +12,13 @@ def index():
 @app.route('/process1', methods=['POST'])
 def run_process1():
     data = request.get_json()
-    result = process1(data)
+    result = process1(data["data"])
     return jsonify({'result': result})
 
 @app.route('/process2', methods=['POST'])
 def run_process2():
     data = request.get_json()
-    result = process2(data)
+    result = process2(data["data"])
     return jsonify({'result': result})
 
 # TODO: Create a flask app with two routes, one for each function.
